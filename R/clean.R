0083e3b5467610a06f424901a5b031757df63347
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-05-29 17:55:32 -0400

    Initial commit - code

---
+++ R/clean.R
@@ -0,0 +1,28 @@
+
+#' Assign sample numbers, and optionally remove some valves' data.
+#'
+#' @param rawdata A \code{data.frame} of data returned by \code{\link{process_directory}}.
+#' @param remove_valves An optional vector of integer valve numbers to remove
+#' from the data, e.g. ambient ports.
+#' @return The (possibly filtered) data with a new \code{Sample_number} column.
+#' @import dplyr
+#' @importFrom tidyr replace_na
+#' @export
+assign_sample_numbers <- function(raw_data, remove_valves = c()) {
+  stopifnot(!"Sample_number" %in% colnames(raw_data))
+  if(length(remove_valves)) {
+    message("Removing valves ", remove_valves)
+  }
+
+  raw_data %>%
+    # Whenever the valve position changes, that's a new sample starting
+    mutate(newsample = MPVPosition != lag(MPVPosition)) %>%
+    replace_na(list(newsample = FALSE)) %>%
+    mutate(Sample_number = cumsum(newsample)) %>%
+    select(-newsample) %>%
+
+    filter(!MPVPosition %in% remove_valves)
+  # group_by(samplenum) %>%
+  # mutate(elapsed_seconds = as.double(difftime(DATETIME, min(DATETIME), units = "secs"))) ->
+  # rawdata_samples
+}
---
+++ R/read_data.R
@@ -0,0 +1,61 @@
+
+
+#' Read a single Picarro output file.
+#'
+#' @param filename Fully qualified filename to read.
+#' @return A \code{data.frame} of the data.
+#' @note This can read plain-text or compressed (via \code{zip} or \code{gz}) files.
+#' @importFrom tibble as_tibble
+read_picarro_file <- function(filename) {
+  message("Reading ", filename)
+  stopifnot(file.exists(filename))
+
+  f <- filename
+  if(grepl(".gz$", filename)) {
+    f <- gzfile(filename)
+  } else if(grepl(".zip$", filename)) {
+    f <- unz(filename)
+  }
+
+  as_tibble(read.table(f, header = TRUE, stringsAsFactors = FALSE, row.names = NULL))
+}
+
+
+#' Scan a directory and read all Picarro files in it.
+#'
+#' @param path Folder to process, character.
+#' @param tz Timezone to use for timestamps, e.g. "America/New_York", character.
+#' See \url{https://en.wikipedia.org/wiki/List_of_tz_database_time_zones}.
+#' @param recursive Scan (via \code{\link{list.files}}) subfolders? Logical.
+#' @return A \code{data.frame} of the data.
+#' @import dplyr
+#' @note This can read plain-text or compressed (via \code{zip} or \code{gz}) files.
+#' @export
+process_directory <- function(path, tz = "", recursive = TRUE) {
+
+  if(tz == "") {
+    warning("Time zone blank and so being set to UTC; is this correct?")
+    tz <- "UTC"
+  }
+
+  filelist <- list.files(path = path,
+                         pattern = "dat$|dat.gz$|dat.zip$",
+                         recursive = recursive,
+                         full.names = TRUE)
+  filedata <- list()
+  message("Found ", length(filelist), " files")
+  for(f in filelist) {
+    read_picarro_file(f) %>%
+
+      # Create DATETIME field and select columns we need
+      mutate(DATETIME = as.POSIXct(paste(DATE, TIME),
+                                   format = "%Y-%m-%d %H:%M:%S", tz = tz)) %>%
+      select(DATETIME, ALARM_STATUS, MPVPosition, CH4_dry, CO2_dry, h2o_reported) %>%
+
+      # Discard any fractional valve numbers
+      filter(MPVPosition == floor(MPVPosition)) ->
+      filedata[[f]]
+  }
+
+  bind_rows(filedata, .id = "Filename")
+}
