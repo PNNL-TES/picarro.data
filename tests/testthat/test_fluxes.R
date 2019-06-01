d948b542367b2f62f552e2bc9b8c1b4eccee0138
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-06-01 07:16:10 -0400

    Test code clarifications

--- tests/testthat/test_fluxes.R
+++ tests/testthat/test_fluxes.R
@@ -13,13 +13,15 @@ test_that("compute_flux", {
   expect_error(compute_flux(1:10, 401:410, 200, tair_C = 20, pressure_kPa = -1))
 
   # Roughly reproduce the example from p. 1-24 of the Licor 8100A manual
-  x <- compute_flux(seconds = seq(7.3, 120, length.out = 10),
-                    gas_ppm = seq(434, 570, length.out = 10),
-                    volume_cm3 = 7385.3,  # LI-8100A system volume
-                    tair_C = 20)
-  x <- x * (10000 / 317.8) # convert to per m2 based on 20 cm chamber
-  # Hmm...we don't know the chamber area from the Licor example
-  # If it's 20 cm, then the flux computed here is ~2x too high
-  # If it was a 27 cm diameter chamber (area = ~600 cm2) then things match
-})
+  x <- compute_flux(
+    seconds = seq(7.3, 120, length.out = 10), # figure 1-3
+    gas_ppm = seq(434, 570, length.out = 10), # figure 1-3
+    volume_cm3 = 7385.3,                      # LI-8100A system volume
+    tair_C = 25                               # "tropical greenhouse"
+  )
+  x <- x * (10000 / 317.8) # convert to per m2 based on 20 cm diameter chamber
 
+  # Hmm...we don't know the chamber area from the Licor example
+  # If it's 20 cm, then the flux computed here is 1.9x too high
+  # If it was a custom 27 cm diameter chamber (area = ~600 cm2) then things match
+})
