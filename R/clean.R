469e6658812af9fc8521544fb1e3e881de8f1884
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-05-31 21:16:26 -0400

    Tests

--- R/clean.R
+++ R/clean.R
@@ -1,20 +1,14 @@
 
-#' Assign sample numbers, and optionally remove some valves' data.
+#' Assign sample numbers.
 #'
 #' @param raw_data A \code{data.frame} of data returned by \code{\link{process_directory}}.
-#' @param remove_valves An optional vector of integer valve numbers to remove
-#' from the data, e.g. ambient ports.
-#' @return The (possibly filtered) data with a new \code{Sample_number} column.
+#' @return The data with new \code{Sample_number} and \code{Elapsed_seconds} columns.
 #' @import dplyr
 #' @importFrom tidyr replace_na
 #' @export
-assign_sample_numbers <- function(raw_data, remove_valves = c()) {
+assign_sample_numbers <- function(raw_data) {
   stopifnot(!"Sample_number" %in% colnames(raw_data))
   stopifnot("MPVPosition" %in% colnames(raw_data))
-
-  if(length(remove_valves)) {
-    message("Removing valves ", remove_valves)
-  }
 
   raw_data %>%
     # Whenever the valve position changes, that's a new sample starting
@@ -22,25 +16,29 @@ assign_sample_numbers <- function(raw_data, remove_valves = c()) {
     replace_na(list(newsample = FALSE)) %>%
     mutate(Sample_number = cumsum(newsample)) %>%
     select(-newsample) %>%
-    # Remove unwanted valves
-    filter(!MPVPosition %in% remove_valves)
-  # group_by(samplenum) %>%
-  # mutate(elapsed_seconds = as.double(difftime(DATETIME, min(DATETIME), units = "secs"))) ->
-  # rawdata_samples
+    # Compute Elapsed_seconds for each sample
+    group_by(Sample_number) %>%
+    mutate(Elapsed_seconds = as.double(difftime(DATETIME, min(DATETIME), units = "secs")))
 }
 
 
-#' Clean the data: create \code{DATETIME} field, remove fractional valves
+#' Clean the data: create \code{DATETIME} field, remove fractional and unwanted valves
 #'
 #' @param raw_data A \code{data.frame} of data returned by \code{\link{process_directory}}.
 #' @param tz Timezone to use for timestamps, e.g. "America/New_York", character.
 #' See \url{https://en.wikipedia.org/wiki/List_of_tz_database_time_zones}.
-#' @return
+#' @param remove_valves An optional vector of integer valve numbers to remove
+#' from the data, e.g. ambient ports.
+#' @return The cleaned data.
 #' @export
-clean_data <- function(raw_data, tz = "") {
+clean_data <- function(raw_data, tz = "", remove_valves = c()) {
   if(tz == "") {
     warning("Time zone blank and so being set to UTC; is this correct?")
     tz <- "UTC"
+  }
+
+  if(length(remove_valves)) {
+    message("Removing valves ", remove_valves)
   }
 
   raw_data %>%
@@ -50,5 +48,7 @@ clean_data <- function(raw_data, tz = "") {
     select(DATETIME, ALARM_STATUS, MPVPosition, CH4_dry, CO2_dry, h2o_reported) %>%
 
     # Discard any fractional valve numbers
-    filter(MPVPosition == floor(MPVPosition))
+    filter(MPVPosition == floor(MPVPosition)) %>%
+    # Remove unwanted valves
+    filter(!MPVPosition %in% remove_valves)
 }
--- tests/testthat/test_clean.R
+++ tests/testthat/test_clean.R
@@ -9,15 +9,37 @@ test_that("assign_sample_numbers", {
   df <- data.frame(MPVPosition = 1, Sample_number = 1)
   expect_error(assign_sample_numbers(df))
 
-  df <- data.frame(MPVPosition = c(4, 4, 2, 1, 1), x = "test")
+  df <- data.frame(DATETIME = seq(as.Date("2000/1/1"), by = "day", length.out = 5),
+                   MPVPosition = c(4, 4, 2, 1, 1), x = "test")
   out <- assign_sample_numbers(df)
   expect_identical(nrow(out), nrow(df))
   expect_true("Sample_number" %in% colnames(out))
   expect_identical(length(unique(out$Sample_number)), 3L)
 
+})
+
+test_that("clean_data", {
+  df <- data.frame(DATE = "2019-05-30", TIME = "19:55:32",
+                   ALARM_STATUS = 0,
+                   MPVPosition = c(4, 4.1, 2, 1, 1),
+                   CO2_dry = 1, CH4_dry = 1, h2o_reported = 1)
+
+  # Generates warning if time zone not specified
+  expect_warning(clean_data(df))
+
+  # Removes fractional valves
+  ints <- sum(df$MPVPosition == floor(df$MPVPosition))
+  out <- clean_data(df, tz = "America/New_York")
+  expect_identical(nrow(out), ints)
+
+  # Creates DATETIME field
+  expect_true("DATETIME" %in% colnames(out))
+  expect_identical(class(out$DATETIME), c("POSIXct", "POSIXt"))
+
   # Removes valves as requested
-  out <- assign_sample_numbers(df, remove_valves = 2)
+  out <- clean_data(df, remove_valves = c(2), tz = "UTC")
   expect_true(!2 %in% out$MPVPosition)
-  out <- assign_sample_numbers(df, remove_valves = c(1, 4))
+  out <- clean_data(df, remove_valves = c(1, 4), tz = "UTC")
   expect_true(all(!c(1, 4) %in% out$MPVPosition))
 })
+
